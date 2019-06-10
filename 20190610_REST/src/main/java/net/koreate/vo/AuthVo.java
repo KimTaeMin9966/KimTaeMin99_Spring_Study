b1f8752e0d5eb0631f0f9992e1228590b0c71167
KimTaeMin99 <ktm06069@naver.com>
2019-06-10 16:58:05 +0900

    lombok활용

--- 20190610_REST/src/main/java/net/koreate/vo/AuthVo.java
+++ 20190610_REST/src/main/java/net/koreate/vo/AuthVo.java
@@ -1,35 +1,13 @@
 package net.koreate.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthVo {
 	private String userid;
 	private String auth;
-	
-	public AuthVo() {}
-	
-	public AuthVo(String userid, String auth) {
-		this.userid = userid;
-		this.auth = auth;
-	}
-
-	public String getUserid() {
-		return userid;
-	}
-
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-	public String getAuth() {
-		return auth;
-	}
-
-	public void setAuth(String auth) {
-		this.auth = auth;
-	}
-
-	@Override
-	public String toString() {
-		return "AuthVo [userid=" + userid + ", auth=" + auth + "]";
-	}
-
 }
